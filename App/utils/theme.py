fb674cca63c6c057bf4edfe713e010a4fc86a862
GrishaTS <bezgrisa@gmail.com>
2025-02-06 11:43:54 +0300

    add searching images

--- App/utils/theme.py
+++
@@ -1,8 +0,0 @@
-from flet import ControlEvent, ThemeMode
-
-def toggle_theme(e: ControlEvent) -> None:
-    '''Переключает тему страницы между светлой и тёмной.'''
-    e.page.theme_mode = (
-        ThemeMode.DARK if e.page.theme_mode == ThemeMode.LIGHT else ThemeMode.LIGHT
-    )
-    e.page.update()
--- clip.py
+++
@@ -1,4 +0,0 @@
-import ruclip
-
-device = 'cuda'
-clip, processor = ruclip.load('ruclip-vit-base-patch32-384', device=device)
